765dad2a9e7d5611770a72db46896dcca1e51ed8
SarfarazAhmed1012 <sarfarazahmed1012@gmail.com>
2021-09-08 22:00:18 +0500

    Redux Form with validation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import react, {Component} from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
+import { ConfigureStore } from './redux/configureStore';
 import './App.css';
+
+const store = ConfigureStore();
  
 class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
-          <div>
-              <Main />
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
           </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </Provider>
 
     );
   }
