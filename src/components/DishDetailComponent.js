dfcfa8866909ec4a553778b989e13931d99619e8
SarfarazAhmed1012 <sarfarazahmed1012@gmail.com>
2021-09-18 11:16:00 +0500

    fetch from server and fetch handling errors

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -19,6 +19,7 @@ import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
 import { FadeTransform, Fade, Stagger } from "react-animation-components";
 import { Loading } from "./LoadingComponent";
+import { baseUrl } from "../shared/baseUrl";
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || val.length <= len;
@@ -137,7 +138,7 @@ function RenderDish({ dish }) {
         }}
       >
         <Card>
-          <CardImg top src={dish.image} alt={dish.name} />
+          <CardImg top src={baseUrl + dish.image} alt={dish.name} />
           <CardBody>
             <CardTitle>{dish.name}</CardTitle>
             <CardText>{dish.description}</CardText>
--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,5 +1,6 @@
 import * as ActionTypes from './ActionTypes';
 import { DISHES } from '../shared/dishes';
+import { baseUrl  } from '../shared/baseUrl'; // for communicating with the Servers
 
 // ACTION //
 // this action will return a plain JS object
@@ -13,13 +14,41 @@ export const addComment = (dishId, rating, author, comment) => ({
     }
 }); // Now we will send this action to the store, and we know it should be affecting the comments part of the state. 
     
+
+// FOR DISHES  
 export const fetchDishes = () => (dispatch) => {
 
     dispatch(dishesLoading(true));
 
-    setTimeout(() => {
-        dispatch(addDishes(DISHES));
-    }, 2000);
+
+    // this was placed originally in order to simulate the communicating with the server.
+    // in place of this we will use actual fetch to communicate.
+    // setTimeout(() => {
+    //     dispatch(addDishes(DISHES));
+    // }, 2000);
+
+    return fetch(baseUrl + 'dishess') // we know dishes are accessible at localhost:3001
+        .then(response => {
+            if (response.ok){
+                return response;
+            }
+            else{ // handling error here
+                var error = new Error('Error' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        }, // incase if server dont reponds 
+        error => {
+            var errmess = new Error(error.message);
+            throw errmess; // this var 'errmess' is would be used in dishesFailed Action below.
+        }
+        )
+        .then(response => response.json())
+        .then(dishes => dispatch(addDishes(dishes)))
+        // if in case we throw an error from above two places and incase if a promise has been rejected
+        // it will not go to above 2 then functions, so here we will catch error below uusing catch function
+        .catch(error => dispatch(dishesFailed(error.message)));
+
 }
 
 export const dishesLoading = () => ({
@@ -34,4 +63,80 @@ export const dishesFailed = (errmess) => ({
 export const addDishes = (dishes) => ({
     type: ActionTypes.ADD_DISHES,
     payload: dishes
+});
+
+
+// FOR COMMENTS
+export const fetchComments = () => (dispatch) => {
+
+    return fetch(baseUrl + 'comments') // we know comments are accessible at localhost:3001
+    .then(response => {
+        if (response.ok){
+            return response;
+        }
+        else{ // handling error here
+            var error = new Error('Error' + response.status + ': ' + response.statusText);
+            error.response = response;
+            throw error;
+        }
+    }, // incase if server dont reponds 
+    error => {
+        var errmess = new Error(error.message);
+        throw errmess; // this var 'errmess' is would be used in dishesFailed Action below.
+    })
+        .then(response => response.json())
+        .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message)));
+}
+
+export const commentsFailed = (errmess) => ({
+    type: ActionTypes.COMMENTS_FAILED,
+    payload: errmess
+});
+
+export const addComments = (comments) => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments
+});
+
+// FOR PROMOS
+
+export const fetchPromos = () => (dispatch) => {
+
+    dispatch(promosLoading(true));
+
+    return fetch(baseUrl + 'promotions') // we know promos are accessible at localhost:3001
+    .then(response => {
+        if (response.ok){
+            return response;
+        }
+        else{ // handling error here
+            var error = new Error('Error' + response.status + ': ' + response.statusText);
+            error.response = response;
+            throw error;
+        }
+    }, // incase if server dont reponds 
+    error => {
+        var errmess = new Error(error.message);
+        throw errmess; // this var 'errmess' is would be used in dishesFailed Action below.
+    }
+    )
+        .then(response => response.json())
+        .then(promos => dispatch(addPromos(promos)))
+        .catch(error => dispatch(promosFailed(error.message)));
+
+}
+
+export const promosLoading = () => ({
+    type: ActionTypes.PROMOS_LOADING
+});
+
+export const promosFailed = (errmess) => ({
+    type: ActionTypes.PROMOS_FAILED,
+    payload: errmess
+});
+
+export const addPromos = (promos) => ({
+    type: ActionTypes.ADD_PROMOS,
+    payload: promos
 });
